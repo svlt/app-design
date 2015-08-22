3a69afc5abdc55f58cd37ccd1a12e64aaa0fa88e
Alan Hardman <alanaktion@gmail.com>
2015-08-21 22:32:35 -0600

    Added node and gulp setup for compiling scss.

--- README.md
+++ README.md
@@ -4,4 +4,12 @@
 
 ## Installation
 
-Clone the repo, then run `bower install` from `assets/`.
+1. Clone the repo
+2. `cd` to `assets/`
+3. Run `bower install`
+
+## Development
+
+1. `cd` to `assets/`
+2. Run `npm install`
+3. Run `gulp scss` to compile the SCSS or just `gulp` to run continuously.
---
+++ assets/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify');
+
+gulp.task('scss', function() {
+	gulp.src('scss/style.scss')
+		.pipe(sass().on('error', notify.onError(function(error) {
+				return 'Error compiling SCSS: ' + error.message;
+			})))
+		.pipe(gulp.dest('css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest('css'))
+		.pipe(notify({
+			message: 'SCSS compiled!'
+		}));
+});
+
+gulp.task('default', ['scss', 'watch']);
+
+gulp.task('watch', function() {
+	gulp.watch('scss/*.scss', ['scss']);
+});
