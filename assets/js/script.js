872f44e7f63fcc383cf82d694b313fb0c5bbab4f
Alan Hardman <alanaktion@gmail.com>
2015-08-22 13:57:04 -0600

    Adding cover photos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 $(function() {
-	$('.card-profile-banner, .profile-banner').geopattern('ken');
+	$('.card-profile-banner, .profile-banner').each(function(i, el) {
+		var $this = $(this);
+		if(!$this.css('background-image')) {
+			$this.geopattern($this.data('username'));
+		}
+	});
 });
